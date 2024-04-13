cb79906dd468ae2891b15910430f7199ba91a716
joao.pedro <joaopedrobueno2910@gmail.com>
2024-04-13 18:57:11 -0300

    restTemplate
    restTemplate

--- hr-payroll/src/main/java/br/com/jpbueno/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/jpbueno/hrpayroll/services/PaymentService.java
@@ -1,12 +1,32 @@
 package br.com.jpbueno.hrpayroll.services;
 
 import br.com.jpbueno.hrpayroll.entities.Payment;
+import br.com.jpbueno.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("João", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class,
+                uriVariables);
+
+        assert worker != null;
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
