eafda00b7e11eb6d204b6ded4f0b0ba2324061f5
bueno <joaopedrobueno2910@gmail.com>
2024-04-15 21:42:32 -0300

    project eureka created ag
    project eureka created ag

--- hr-eureka-server/src/main/java/com/devsuperior/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/devsuperior/hreurekaserver/HrEurekaServerApplication.java
@@ -1,8 +1,10 @@
-package br.com.jpbueno.hreurekaserver;
+package com.devsuperior.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
